c34a71b68a533dc6abd7d7ab55610d55793ab8c1
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-03-22 11:06:17 +0300

    added authetication witn facebook & twitter

--- Lections/Lections/Controllers/AuthorizationController.cs
+++ Lections/Lections/Controllers/AuthorizationController.cs
@@ -2,15 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lections.Controllers
 {
     public class AuthorizationController : Controller
     {
+        private readonly IAuthenticationSchemeProvider asp;
+
+        public AuthorizationController(IAuthenticationSchemeProvider a)
+        {
+            this.asp = a;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<IActionResult> SignOut()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Sign(String provider)
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, provider);
+        }
+
+        public async Task<IActionResult> Login()
+        {
+            var allSchemeProvider = (await asp.GetAllSchemesAsync())
+               .Select(n => n.DisplayName).Where(n => !String.IsNullOrEmpty(n));
+
+
+            return View("Index",allSchemeProvider);
         }
 
         public IActionResult SignUp()
--- Lections/Lections/Startup.cs
+++ Lections/Lections/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,24 @@ namespace Lections
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            })
+          .AddFacebook(Foptions =>
+          {
+              Foptions.AppId = "783478808509094";
+              Foptions.AppSecret = "603a11fb352f6693d9185538a49943fa";
+          })
+          .AddTwitter(Toptions =>
+          {
+              Toptions.ConsumerKey = "K4rGUY6SwKPcvCcm73TNFDMgr";
+              Toptions.ConsumerSecret = "7v87qSbvOu4KIAVoVzwgYgN3q3oEutE0QVjz2xnrz9CJGXSIzK";
+          })
+          .AddCookie();
+
             services.AddMvc();
         }
 
@@ -37,6 +56,7 @@ namespace Lections
                 app.UseExceptionHandler("/Home/Error");
             }
 
+            app.UseAuthentication();
             app.UseStaticFiles();
 
             app.UseMvc(routes =>
