8b486294009db66c85262dc744be64f696a5f915
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-04-02 01:59:52 +0300

    added lection model

---
+++ Lections/Lections/Models/Lection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lections.Models
+{
+    public class Lection
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public string smallDescription { set; get; }
+        public string text { set; get; }
+        public int stars { set; get; }
+        public int userId { set; get; }
+    }
+}
