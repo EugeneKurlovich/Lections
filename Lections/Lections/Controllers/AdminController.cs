167cce541688c42adf4e7e6b5aac4104cb634123
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2018-04-01 22:58:51 +0300

    all users

--- Lections/Lections/Controllers/AdminController.cs
+++ Lections/Lections/Controllers/AdminController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lections.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lections.Controllers
 {
     public class AdminController : Controller
     {
+        private static DatabaseContext db;
+
+        public AdminController(DatabaseContext context)
+        {
+            db = context;
+        }
+
         public IActionResult Users()
         {
-            return View();
+            return View(db.Users);
         }
 
         public IActionResult Lections()
